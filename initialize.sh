6f307fb2e51763e0cacb2c0f97757480c8a3e2c7
Greg Sarjeant <gsarjeant@gmail.com>
2018-08-28 17:45:06 -0400

    Complete unix validation functions.

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# NiOTE - I'm just using 1 as a universal nonzero return here for convenience.
+#         I can put in more robust failure state reporting if needed later.
+
 ######################################################################################
 # Function definition
 ######################################################################################
@@ -27,29 +30,121 @@ usage(){
 #    - exist
 #    - not be this project's root directory
 validate-targetdir(){
-# First, see if targetdir was passed in (i.e. TARGET_DIR is set)
+    # We'll assume target dir is valid until something fails
+    TARGET_DIR_VALID=0
+
+    # First, see if targetdir was passed in (i.e. TARGET_DIR is set)
     # If it wasn't, then set it to the current directory
-    if [ -n ${TARGET_DIR+x} ] ; then
+    if [[ -z "$TARGET_DIR" ]] ; then
         echo "Target dir not set. Setting to current directory"
         TARGET_DIR=$PWD
     fi
 
-    echo "Target dir set to ${TARGET_DIR}"
-
-    # Check whether TARGET_DIR exists after setting it and return the result
-    # (true if TAARGET_DIR exists, false otherwise)
-    [ -d "${TARGET_DIR}" ]
+    # Check whether TARGET_DIR exists after setting
+    # If it doesn't, print a message and set the return value to 1
+    if [[ ! -d "$TARGET_DIR" ]] ; then
+        TARGET_DIR_VALID=1
+        echo "ERROR: Target directory (${TARGET_DIR}) does not exist."
+    fi	
 
     # Make sure that TARGET_DIR is not this script's parent directory
+    # Get the directory of this script
+    MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+    if [[ "$TARGET_DIR" == "$MY_DIR" ]] ; then
+	TARGET_DIR_VALID=1
+        echo "ERROR: Target directory is this script's directory"
+    fi
+
+    return $TARGET_DIR_VALID
 }
+
+# If no python version was specified, make sure there is a version of python installed
+# If a specific version was specified, make sure that version exists.
+validate-python(){
+    if [[ -z "$PYTHON_VERSION" ]] ; then
+        # No python version was specified.
+	# Try to find the system python (`which python`) and use it if it exists.
+        echo "No python version specified. Using system python."
+	PYTHON_VERSION=$( which python )
+	PYTHON_INSTALLED=$?
+
+	if (( $PYTHON_INSTALLED != 0 )) ; then
+            # `which python` returned an error. Python is not installed (or at least isn't on the PATH).
+	    # Print an error message and return non-zero
+            echo "ERROR: python could not be found. Please ensure that python is installed and is in the PATH."
+	    return 1
+	fi
+    else
+        # The user requested a specific python.
+	# Verify that it exists.
+	if [[ ! -f "$PYTHON_VERSION" ]] ; then
+	    # The requested version of python doesn't exist.
+	    # Print an error message and exit
+	    echo "ERROR: The specified version of python could not be found."
+	    echo "       ${PYTHON_VERSION}"
+	    return 1
+	fi
+    fi
+}
+
+# Ensure that virtualenv is installed
+validate-virtualenv(){
+  # Nothing fancy here. We can just call "which virtualenv" and piggyback off the return code.
+  which virtualenv >/dev/null
+}
+
+# Ensure that git is installed
+validate-git(){
+  # Nothing fancy here. We can just call "which git" and piggyback off the return code.
+  which git >/dev/null
+}
+
+# Call the individual functions to validate that all prereqs are met.
+# Return 0 on success and 1 if there are any failures
+validate-prereqs(){
+
+    # validate-targetdir and validate-python will print messages on failure
+    # So we don't need to print anything extra here.
+    # Just capture the return values
+
+    validate-targetdir
+    TARGETDIR_RESULT=$?
+    
+    validate-python
+    PYTHON_RESULT=$?
+
+    # validate-virtualenv and validate-git will not print messages on failure
+    # So we need to print error messages here if they fail
+
+    validate-virtualenv
+    VIRTUALENV_RESULT=$?
+    if (( VIRTUALENV_RESULT != 0 )); then
+        echo "ERROR: virtualenv was not found"
+    fi
+
+    validate-git
+    GIT_RESULT=$?
+    if (( GIT_RESULT != 0 )) ; then
+        echo "ERROR: git was not found."
+    fi
+
+    # Add up all the return values and return the result.
+    return $(( TARGETDIR_RESULT + PYTHON_RESULT + VIRTUALENV_RESULT + GIT_RESULT))
+}
+
 ######################################################################################
 # Script execution
 ######################################################################################
 
+# The script allows short and long arguments, but use getopts to parse arguments.
+# getoprt only deals with short arguments, 
+# so convert any long arguments to short arguments before evaluating
+
 # Create an array to hold args
 args=()
 
-#replace long arguments with their short versions
+# loop through the input arguments and convert long args to short args
 for arg; do
     case "$arg" in
         --help)              args+=( -h ) ;;
@@ -67,9 +162,9 @@ done
 set -- "${args[@]}"
 #printf 'args after update  : '; printf '%q ' "$@"; echo
 
+# parse input arguments with getopts.
 while getopts "hd:p:l:t:c:" OPTION; do
     : "$OPTION" "$OPTARG"
-    #echo "optarg: ${OPTARG}"
 
     case $OPTION in
         h) usage; exit 0;;
@@ -78,7 +173,6 @@ while getopts "hd:p:l:t:c:" OPTION; do
 	l) PYLINT_VERSION="$OPTARG";;
 	t) PYTEST_VERSION="$OPTARG";;
 	c) COVERAGE_VERSION="$OPTARG";;
-	:) echo "Invalid option: ${OPTION} requires an argument" 1>&2 ; exit 1;; 
 	*) echo "Unexpected argument ${OPTION}" ; usage ; exit 1;;
     esac
 done
@@ -86,13 +180,20 @@ done
 # remove all arguments that were handled by getopts from $@
 shift $((OPTIND - 1))
 
-validate-targetdir
+# Validate prerequisites
+validate-prereqs
+VALIDATION_RESULT=$?
 
-echo "Target dir: ${TARGET_DIR}"
-echo "Python version: ${PYTHON_VERSION}"
-echo "Pylint version: ${PYLINT_VERSION}"
-echo "Pytest version: ${PYTEST_VERSION}"
-echo "Coverage version: ${COVERAGE_VERSION}"
-
-
-exit
+# If all prereqs are met, then initialize this directory as a python project
+if (( VALIDATION_RESULT == 0 )) ; then
+    # Just print out the input arguments for now.
+    echo "Target dir: ${TARGET_DIR}"
+    echo "Python version: ${PYTHON_VERSION}"
+    echo "Pylint version: ${PYLINT_VERSION}"
+    echo "Pytest version: ${PYTEST_VERSION}"
+    echo "Coverage version: ${COVERAGE_VERSION}"
+else
+    # Print out a generic error message and quit
+    echo "ERROR: Validation errors detected. Please correct before proceeding."
+    exit 1
+fi
